419a7eee5ab1a4b0d10329062fcdd1e700de60de
YifanZou-8823 <yz12831@nyu.edu>
2025-09-16 23:01:17 -0700

    WK2HW

---
+++ WK2HW/script.js
@@ -0,0 +1,9 @@
+document.getElementById('showImageBtn').addEventListener('click', function() {
+    document.getElementById('imageSection').style.display = 'block';
+});
+
+document.getElementById('clickableImage').addEventListener('click', function() {
+    // Go to Page 2
+    window.location.href = 'page2.html';
+});
+
