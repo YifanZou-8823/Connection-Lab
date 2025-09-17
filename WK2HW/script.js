01de6cd2e4fc3a22f803e6678e2abe5f8687ffb9
YifanZou-8823 <yz12831@nyu.edu>
2025-09-16 23:10:19 -0700

    Delete WK2HW directory

--- WK2HW/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('showImageBtn').addEventListener('click', function() {
-    document.getElementById('imageSection').style.display = 'block';
-});
-
-document.getElementById('clickableImage').addEventListener('click', function() {
-    // Go to Page 2
-    window.location.href = 'Page2.html';
-});
-
